24d5762606d052a2ab6c7d22cde54ec96d87562e
0308sue <b0107701@naver.com>
2025-04-03 12:31:11 +0900

    0403

---
+++ oop_6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+ 
+using namespace std;
+
+int main()
+{
+    int n, sum=0;
+
+    ifstream fin;
+    fin.open("aaa.txt");
+
+    while (fin >> n)
+    {
+        sum += n;
+    }
+    
+
+    fin.close();
+
+    cout <<  "sum의 값은 " << sum <<  endl;
+    
+    return 0;
+
+}
