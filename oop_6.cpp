91f25437d9bb62d16dfb1844383feff1eb216a38
0308sue <b0107701@naver.com>
2025-04-03 13:05:51 +0900

    0403 과제

--- oop_6.cpp
+++ oop_6.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <iomanip>
  
 using namespace std;
 
 int main()
 {
-    int n, sum=0;
+    string name, mname;
+    double score, sum =0, max = -200;
+    int cnt=0;
 
     ifstream fin;
-    fin.open("aaa.txt");
+    fin.open("scores.txt");
 
-    while (fin >> n)
+    if(!fin){
+        cerr << "Error opening scores.txt" << endl;
+        return -1;
+    } 
+
+    ofstream fout("results.txt");
+    if(!fout){
+        cerr << "Error opening result.txt" << endl;
+        return -1;
+    } 
+
+    while (fin >> name >> score)
     {
-        sum += n;
-    }
+        cnt ++;
+        sum += score;
+        if (score > max)
+        {
+            max = score ;
+            mname = name ;
+        }
     
+    }
+
+    fout << fixed << setprecision(2) << showpoint;
+    fout << "총 " << cnt << " 명" << endl;
+    fout << "합계 : " << sum << endl;
+    fout << "평균 : " << sum/cnt << endl;
+    fout << "최고점 : " << mname << " " << max << endl;
 
     fin.close();
+    fout.close();
 
-    cout <<  "sum의 값은 " << sum <<  endl ;
-    
+    cout <<  "Results saved in result.txt " <<  endl ;
     
     return 0;
 
