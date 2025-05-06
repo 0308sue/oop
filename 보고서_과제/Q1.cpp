da522a8931266e0aeede4cd0a6549c177af3f5b2
0308sue <98823240+0308sue@users.noreply.github.com>
2025-05-06 12:46:35 +0900

    Create Q1.cpp

---
+++ 보고서_과제/Q1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, enc = 0, onc = 0, ens = 0, ons = 0;
+    do{
+        cin >> n; 
+        if(n == 0) break;
+        if(n % 2 == 0){
+            enc ++;
+            ens += n;
+        }else{
+            onc ++;
+            ons += n;
+        }
+        
+    }while(n != 0);
+    
+    cout << enc << " " << (float)ens / enc << " " 
+    << onc << " " << (float)ons / onc ;
+    return 0;
+}
